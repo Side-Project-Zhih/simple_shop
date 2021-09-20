8c0fb765f1ad4155f9efb15ca90144064e23ec09
zhih <fufong79570@gmail.com>
2021-09-20 20:40:17 +0800

    feature: add routes of cart and create cartController、cart page

---
+++ controllers/cartController.js
@@ -0,0 +1,12 @@
+const Cart = require('../models/cart')
+module.exports = {
+  renderCart:async (req, res) => {
+
+  },
+  postCart: async(req, res) => {
+
+  },
+  putCart: async(req, res) => {
+    
+  }
+}
---
+++ routes/modules/cart.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const cartController = require('../../controllers/cartController')
+router.get('/',cartController.renderCart)
+router.post('/', cartController.postCart)
+router.put('/',cartController.putCart)
+module.exports = router
