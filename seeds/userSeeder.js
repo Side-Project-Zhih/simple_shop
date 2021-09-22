936ca8eb1ea62f3a723c188b40ffe651099b58c2
zhih <fufong79570@gmail.com>
2021-09-22 23:07:26 +0800

    feature: add admin User

--- seeds/userSeeder.js
+++ seeds/userSeeder.js
@@ -1,14 +1,22 @@
 const db = require('../config/mongoose')
 const bcrypt  = require('bcryptjs')
 const User = require('../models/user')
+let password = '12345678'
 const userData = Array.from({length:5}).map((_,i) => {
-  let password = '12345678'
   password = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
   return {
     name :  `user${i+1}`,
     password,
     email: `user${i+1}@example.com`
   }
+})
+userData[0].isValid = true
+userData.push({
+  name: 'root',
+  email: 'root@example.com',
+  password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+  isValid: true,
+  role:'admin'
 })
 db.once('open', async()=> {
   await User.insertMany(userData)
